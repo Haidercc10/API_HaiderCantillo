8889710d15b9ccbe408dcf5e21048db9f9aa3100
Háider Cantillo Colón <cantillohaider10@gmail.com>
2024-08-24 18:34:33 -0500

    Cambios
    Adición del array de objetos para visualizar en formato JSON

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/personas', methods=['GET'])
 def get_personas():
-    personas = ["Abby", "Haider", "Daniel", "Felipe", "Mary"]
+    personas = [
+        { "nombre" : "Abby" },
+        { "nombre" : "Haider" }, 
+        { "nombre" : "Daniel" },    
+        { "nombre" : "Felipe" },    
+        { "nombre" : "Mary" }   
+    ]
     return jsonify(personas)
 
 if __name__ == '__main__':
